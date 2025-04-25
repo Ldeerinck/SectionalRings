0357bd04600e6be6c97db4f5852c2552d6206a99
Ldeerinck <ldeerinck2@me.com>
2025-04-25 12:40:38 -0700

    Initial Commit

---
+++ SectionalRings/SectionalRingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SectionalRingsApp.swift
+//  SectionalRings
+//
+//  Created by Luke Deerinck on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SectionalRingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SectionalRingsTests/SectionalRingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SectionalRingsTests.swift
+//  SectionalRingsTests
+//
+//  Created by Luke Deerinck on 4/25/25.
+//
+
+import Testing
+@testable import SectionalRings
+
+struct SectionalRingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
